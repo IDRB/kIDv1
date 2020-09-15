a4a0cebf2ec9b46914cbfde59fe36f7d02aea986
Bo Lee <libcell@cqu.edu.cn>
2020-09-15 13:49:20 +0800

    Update Chunk00-gettting the annotation file from website.R

--- Chunk00-gettting the annotation file from website.R
+++ Chunk00-gettting the annotation file from website.R
@@ -1,4 +1,17 @@
 
+################################################################################
+#    &&&....&&&    % Project: Mapping KO ids to gene symbols  #
+#  &&&&&&..&&&&&&  % Authors: Xiner Nie, Bo Li, Jingxin Tao, Hao He            #
+#  &&&&&&&&&&&&&&  % Date: Aug. 18th, 2020                                     #
+#   &&&&&&&&&&&&   %                                                           #
+#     &&&&&&&&     % Environment: R version 3.6.0;                             #
+#       &&&&       % Platform: x86_64-w64-mingw32/x64 (64-bit)                 #
+#        &         %                                                           #
+################################################################################
+
+### ****************************************************************************
+### code chunk number 10: Evaluation Criteria for data clustering.
+### ****************************************************************************
 
 download.file("http://rest.kegg.jp/list/ko", "ko.txt")
 
@@ -11,7 +24,7 @@ strsplit(strsplit(anno[1], "\t")[[1]][2], "; ")[[1]]
 
 strsplit(strsplit(strsplit(anno[1], "\t")[[1]][2], "; ")[[1]][2], " \\[")[[1]][1]
 
-strsplit(strsplit(strsplit(anno[1], "\t")[[1]][2], "; ")[[1]][2], " \\[")[[1]][2]
+gsub("EC", "\\[EC", strsplit(strsplit(strsplit(anno[1], "\t")[[1]][2], "; ")[[1]][2], " \\[")[[1]][2])
 
 
 
